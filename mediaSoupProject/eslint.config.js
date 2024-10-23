3bcb5f2023960026576d2440981178dba364e4ec
Yeom95 <amadeusw@DESKTOP-U73TVLR>
2024-10-23 14:33:37 +0900

    add camera function

---
+++ mediaSoupProject/eslint.config.js
@@ -0,0 +1,48 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+    { ignores: ['dist'] },
+    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                ecmaFeatures: { jsx: true },
+                sourceType: 'module',
+            },
+        },
+        settings: { react: { version: '18.3' } },
+        plugins: {
+            react,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...react.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
+            ...reactHooks.configs.recommended.rules,
+            'react/jsx-no-target-blank': 'off',
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            'semi': ['error', 'always'], // 세미콜론 강제 사용
+            'quotes': ['error', 'single'], // 싱글 쿼트 사용 강제
+            //'no-console': ['warn'], // 콘솔 로그 경고
+            'indent': ['error', 4], // 2칸 들여쓰기
+            'no-var': 'error', //var 대신 let/const 사용 강제
+            'arrow-parens': ['error', 'always'], //일관성을 위해 파라미터가 하나일때라도 괄호 사용
+            'object-curly-spacing': ['error', 'always'], //중괄호 내 공백 강제,
+            'no-trailing-spaces': 'error', //불필요한 공백 제거
+            'comma-dangle': ['error', 'always-multiline'], //후행 콤마 사용
+            'curly': ['error', 'all'], //모든 조건문에 중괄호 사용 강제
+        },
+    },
+];
---
+++ mediaSoupProject/public/index.js
@@ -0,0 +1,42 @@
+//clientside
+const io = require('socket.io-client');
+
+const socket = io('/mediasoup');
+
+socket.on('connection-success',(socketId) => {
+    console.log(socketId);
+});
+
+let params ={
+//mediasoup params
+};
+
+const streamSuccess = async(stream) => {
+    localVideo.srcObject = stream;
+    const track = stream.getVideoTracks()[0];
+    params = {
+        track,
+        ...params,
+    };
+};
+
+const getLocalStream = () => {
+    navigator.getUserMedia({
+        audio: false,
+        video: {
+            width:{
+                min:640,
+                max:1920,
+            },
+            height:{
+                min:400,
+                max:1080,
+            },
+        },
+    },streamSuccess,(error)=>{
+        console.log(error.message);
+    });
+};
+
+const btnLocalVideo = document.getElementById('btnLocalVideo');
+btnLocalVideo.addEventListener('click',getLocalStream);
